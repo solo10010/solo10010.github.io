5ffff162ae03d26483277235ba81d8770d544cb0
solo10010 <tohac2011@gmail.com>
2021-10-22 00:46:32 +0600

    add apexcharts

---
+++ assets/js/solojs.js
@@ -0,0 +1,289 @@
+var options = {
+    chart: {
+      height: 180,
+      type: "radialBar",
+    },
+  
+    series: [70],
+    colors: ["#12C7A9"],
+    plotOptions: {
+      radialBar: {
+        hollow: {
+          margin: 0,
+          size: "65%",
+          background: "#212529"
+        },
+        track: {
+          dropShadow: {
+            enabled: true,
+            top: 2,
+            left: 0,
+            blur: 4,
+            opacity: 0.15
+          }
+        },
+        dataLabels: {
+          name: {
+            offsetY: -10,
+            color: "#12C7A9",
+            fontSize: "20px"
+          },
+          value: {
+            color: "#fff",
+            fontSize: "18px",
+            show: true
+          }
+        }
+      }
+    },
+    fill: {
+      type: "gradient",
+      gradient: {
+        shade: "dark",
+        type: "vertical",
+        gradientToColors: ["#12C7A9"],
+        stops: [0, 100]
+      }
+    },
+    stroke: {
+      lineCap: "round"
+    },
+    labels: ["Python 3"]
+  };
+  
+  var chart = new ApexCharts(document.querySelector("#chart"), options);
+  
+  
+  chart.render();
+
+  var options = {
+    chart: {
+      height: 180,
+      type: "radialBar",
+    },
+  
+    series: [65],
+    colors: ["#12C7A9"],
+    plotOptions: {
+      radialBar: {
+        hollow: {
+          margin: 0,
+          size: "65%",
+          background: "#212529"
+        },
+        track: {
+          dropShadow: {
+            enabled: true,
+            top: 2,
+            left: 0,
+            blur: 4,
+            opacity: 0.15
+          }
+        },
+        dataLabels: {
+          name: {
+            offsetY: -10,
+            color: "#12C7A9",
+            fontSize: "20px"
+          },
+          value: {
+            color: "#fff",
+            fontSize: "18px",
+            show: true
+          }
+        }
+      }
+    },
+    fill: {
+      type: "gradient",
+      gradient: {
+        shade: "dark",
+        type: "vertical",
+        gradientToColors: ["#12C7A9"],
+        stops: [0, 100]
+      }
+    },
+    stroke: {
+      lineCap: "round"
+    },
+    labels: ["Bash"]
+  };
+  
+  var chart = new ApexCharts(document.querySelector("#chart2"), options);
+  
+  
+  chart.render();
+
+  var options = {
+    chart: {
+      height: 180,
+      type: "radialBar",
+    },
+  
+    series: [20],
+    colors: ["#12C7A9"],
+    plotOptions: {
+      radialBar: {
+        hollow: {
+          margin: 0,
+          size: "65%",
+          background: "#212529"
+        },
+        track: {
+          dropShadow: {
+            enabled: true,
+            top: 2,
+            left: 0,
+            blur: 4,
+            opacity: 0.15
+          }
+        },
+        dataLabels: {
+          name: {
+            offsetY: -10,
+            color: "#12C7A9",
+            fontSize: "20px"
+          },
+          value: {
+            color: "#fff",
+            fontSize: "18px",
+            show: true
+          }
+        }
+      }
+    },
+    fill: {
+      type: "gradient",
+      gradient: {
+        shade: "dark",
+        type: "vertical",
+        gradientToColors: ["#12C7A9"],
+        stops: [0, 100]
+      }
+    },
+    stroke: {
+      lineCap: "round"
+    },
+    labels: ["JS"]
+  };
+  
+  var chart = new ApexCharts(document.querySelector("#chart3"), options);
+  
+  
+  chart.render();
+
+  var options = {
+    chart: {
+      height: 180,
+      type: "radialBar",
+    },
+  
+    series: [67],
+    colors: ["#12C7A9"],
+    plotOptions: {
+      radialBar: {
+        hollow: {
+          margin: 0,
+          size: "65%",
+          background: "#212529"
+        },
+        track: {
+          dropShadow: {
+            enabled: true,
+            top: 2,
+            left: 0,
+            blur: 4,
+            opacity: 0.15
+          }
+        },
+        dataLabels: {
+          name: {
+            offsetY: -10,
+            color: "#12C7A9",
+            fontSize: "20px"
+          },
+          value: {
+            color: "#fff",
+            fontSize: "18px",
+            show: true
+          }
+        }
+      }
+    },
+    fill: {
+      type: "gradient",
+      gradient: {
+        shade: "dark",
+        type: "vertical",
+        gradientToColors: ["#12C7A9"],
+        stops: [0, 100]
+      }
+    },
+    stroke: {
+      lineCap: "round"
+    },
+    labels: ["Html"]
+  };
+  
+  var chart = new ApexCharts(document.querySelector("#chart4"), options);
+  
+  
+  chart.render();
+
+  var options = {
+    chart: {
+      height: 180,
+      type: "radialBar",
+    },
+  
+    series: [67],
+    colors: ["#12C7A9"],
+    plotOptions: {
+      radialBar: {
+        hollow: {
+          margin: 0,
+          size: "65%",
+          background: "#212529"
+        },
+        track: {
+          dropShadow: {
+            enabled: true,
+            top: 2,
+            left: 0,
+            blur: 4,
+            opacity: 0.15
+          }
+        },
+        dataLabels: {
+          name: {
+            offsetY: -10,
+            color: "#12C7A9",
+            fontSize: "20px"
+          },
+          value: {
+            color: "#fff",
+            fontSize: "18px",
+            show: true
+          }
+        }
+      }
+    },
+    fill: {
+      type: "gradient",
+      gradient: {
+        shade: "dark",
+        type: "vertical",
+        gradientToColors: ["#12C7A9"],
+        stops: [0, 100]
+      }
+    },
+    stroke: {
+      lineCap: "round"
+    },
+    labels: ["css"]
+  };
+  
+  var chart = new ApexCharts(document.querySelector("#chart5"), options);
+  
+  
+  chart.render();
